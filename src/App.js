5e679a805af1c85c64b73b57ba7a643d024d57e2
joseseverinf <89672522+joseseverinf@users.noreply.github.com>
2021-10-26 22:59:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="container-fluid">
+        <h1>Hello Dojo!</h1>
+        <p>Things I need to do:</p>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default App;
